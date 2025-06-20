bfc5d993019bad7199fddd0de2a75449293c6fe0
whals <whalsdud0131@gmail.cm>
2025-06-20 14:35:15 +0900

    Runnable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+class SumRunnable implements Runnable {
+    private final int[] numbers;
+
+    public SumRunnable(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run() {
+        int sum = 0;
+        for (int n : numbers) {
+            sum += n;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " - Sum : " + sum);
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        int[][] dataSets = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 30},
+                {7, 14, 21, 28},
+                {100, 200, 300, 400}
+        };
+        for (int i = 0; i < dataSets.length; i++) {
+            Thread sumThread = new Thread(new SumRunnable(dataSets[i]));
 
+            sumThread.start();
+        }
     }
 }
